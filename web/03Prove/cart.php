cd89862de8eda1aede60152ac2177155a6162bb3
Alex <god18007@byui.edu>
2020-01-25 14:31:20 -0700

    update files

--- web/03Prove/cart.php
+++ web/03Prove/cart.php
@@ -1,11 +1,28 @@
 <?php
-session_start();
-
-$username = $_POST["user_name"];
-$_SESSION["user_name"] = $username;
-
-$new_page = "hello.php";
-header("Location: $new_page");
-die();
-
+$username = htmlspecialchars($_POST['username']);
+$email = htmlspecialchars($_POST['emailAddress']);
+$major = htmlspecialchars($_POST['major']);
+$comments = htmlspecialchars($_POST['comments']);
+$cart = ($_POST['item']);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>Your Cart:</h1>
+<ul>
+<?
+foreach ($cart as $cart){
+    $$cart=htmlspecialchars($cart);
+    echo "<li><p>$cart</p></li>";
+}
+?>
+</ul>
+<h1> Comments: <?php echo $comments; ?></h1>
+
+</body>
+
+</html>
